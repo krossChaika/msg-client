08726cd5915264c716a764a1cfb94cdc38670ee5
kross <kirillchaikovsky8@gmail.com>
2026-07-13 23:59:43 +0300

    centering fix

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -2,8 +2,9 @@ import { Link } from 'react-router';
 
 export default function Home() {
     // max-w-[100vw] w-[40%] min-w-[600px]
+    // w-[max(800px,40%)]
     return (
-        <main className={'w-[max(800px,40%)] max-w-[100vw] p-4'}>
+        <main className={'max-w-[100vw] w-200 m-auto p-4'}>
             <h1 className={'w-full text-center mt-4'}>Welcome!</h1>
             <hr />
             <ul className={'text-blue-500 underline space-y-8 mt-4'}>
